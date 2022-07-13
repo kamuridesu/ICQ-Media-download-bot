b3b63a90bf1879a8209276fd455f8d451d942aba
kamuridesu <myk.gata14@gmail.com>
2022-07-13 18:11:18 -0300

    changes

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
-from ICQBot import ICQBot, Dispatcher
+from ICQBot import ICQBot, Dispatcher, executor
 from ICQBot.messages import ReceivedMessage
+
 from random import randint
 import os
 import sys
+from zipfile import ZipFile
+from tempfile import NamedTemporaryFile
 import logging
-import asyncio
 
-logging.basicConfig(filename="logs.log", level=logging.INFO)
-
+logging.basicConfig(filename="log.log", level=logging.INFO, datefmt="%Y-%m-%d,%H:%M:%S")
 
 bot = ICQBot("")
 dp = Dispatcher(bot)
@@ -18,24 +19,36 @@ for arg in args:
         path = arg.split("--path=")[1]
 
 
-def generateRandomFilename(ext):
-    return str(randint(0, 99999999)) + f".{ext}"
+def generateRandomFilename():
+    return str(randint(0, 99999999))
+
+
+def zipFile(filename, _bytes, ext) -> bytes:
+    print("ziping")
+    with ZipFile(filename + ".zip", "w") as _zip:
+        with NamedTemporaryFile("wb", suffix=ext) as f:
+            f.write(_bytes)
+            _zip.write(f.name)
 
 
 def saveFile(data: bytes, _type: str):
     if _type == "image":
-        _type = "jpg"
+        _type = ".jpg"
     elif _type == "video":
-        _type = "mp4"
-    rd_fn = generateRandomFilename(_type)
+        _type = ".mp4"
+    rd_fn = generateRandomFilename()
     if not os.path.isdir(path):
         os.makedirs(path)
-    while os.path.isfile(os.path.join(path, rd_fn)):
-        rd_fn = generateRandomFilename(_type)
+    while os.path.isfile(os.path.join(path, rd_fn + ".zip")):
+        rd_fn = generateRandomFilename()
     full_path = os.path.join(path, rd_fn)
     logging.info(full_path)
-    with open(full_path, "wb") as f:
-        f.write(data)
+    data = zipFile(full_path, data, _type)
+
+
+@dp.message_handler(commands="/start")
+def start(message: ReceivedMessage):
+    return message.reply("Olá! Me envie uma imagem ou video para baixar!")
 
 
 @dp.message_handler()
@@ -44,13 +57,13 @@ async def getAllMedia(message: ReceivedMessage) -> None:
         for p in message.payloads:
             if p.type == "file":
                 try:
-                    msg = message.reply("Downloading...")
-                    saveFile(bot.downloadFile(p.payload.file_id), p.payload.type)
-                    return msg.edit("media download successful")
+                    msg = await message.reply("Downloading...")
+                    saveFile(await bot.downloadFile(p.payload.file_id), p.payload.type)
+                    return await msg.edit("media download successful")
                 except Exception:
-                    logging.error(p.payload.file_id)
-                    return msg.edit("media download failed!")
+                    logging.error("Failed " + p.payload.file_id)
+                    return await msg.edit(f"Dowload of {p.payload.file_id} failed")
 
 
 if __name__ == "__main__":
-    asyncio.run(dp.start_polling())
+    executor(dp)
